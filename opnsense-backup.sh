a79b60a0c3218038330a88aba623040363c1fd97
ARC-XX <76841018+ARC-XX@users.noreply.github.com>
2023-03-05 12:21:02 +0100

    Add files via upload
    upload shell script prewirtten and testet.

---
+++ opnsense-backup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# set variables for webcalls
+KEY="API Key"
+SECRET="API Secret"
+HOST="FQDN or IP of Host u want to Backup"
+PATHCONFIG="Path were the backupfiles should be stored"
+DATE=$(date +%Y%m%d)
+
+# make the api request for config as xml and alias as json
+curl -s -u "${KEY}":"${SECRET}" https://"${HOST}"/api/backup/backup/download -o "$PATHCONFIG"/opnsense/opnsense-config-"${DATE}".xml
+curl -s -u "${KEY}":"${SECRET}" https://"${HOST}"/api/firewall/alias/export | jq > "$PATHCONFIG"/opnsense/opnsense-alias-"${DATE}".json
+
+# check the backup destination for backups older then 14 days
+find "${PATHCONFIG}" -type f -name opnsense-config\*.xml -mtime +14 -delete
+find "${PATHCONFIG}" -type f -name opnsense-alias\*.json -mtime +14 -delete
