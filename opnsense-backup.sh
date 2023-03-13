21c4a8d913af2f963f00310c46f30e9203a0a8b8
ARC-XX <76841018+ARC-XX@users.noreply.github.com>
2023-03-13 20:16:45 +0100

    Update opnsense-backup.sh

--- opnsense-backup.sh
+++ opnsense-backup.sh
@@ -8,9 +8,9 @@ PATHCONFIG="Path were the backupfiles should be stored"
 DATE=$(date +%Y%m%d)
 
 # make the api request for config as xml and alias as json
-curl -s -u "${KEY}":"${SECRET}" https://"${HOST}"/api/backup/backup/download -o "$PATHCONFIG"/opnsense/opnsense-config-"${DATE}".xml
-curl -s -u "${KEY}":"${SECRET}" https://"${HOST}"/api/firewall/alias/export | jq > "$PATHCONFIG"/opnsense/opnsense-alias-"${DATE}".json
+curl -s -u "${KEY}":"${SECRET}" https://"${HOST}"/api/backup/backup/download -o "${PATHCONFIG}"/opnsense/opnsense-config-"${DATE}".xml
+curl -s -u "${KEY}":"${SECRET}" https://"${HOST}"/api/firewall/alias/export | jq > "${PATHCONFIG}"/opnsense/opnsense-alias-"${DATE}".json
 
 # check the backup destination for backups older then 14 days
-find "${PATHCONFIG}" -type f -name opnsense-config\*.xml -mtime +14 -delete
-find "${PATHCONFIG}" -type f -name opnsense-alias\*.json -mtime +14 -delete
+find "${PATHCONFIG}" -type f -name "${HOST}"-config\*.xml -mtime +14 -delete
+find "${PATHCONFIG}" -type f -name "${HOST}"-alias\*.json -mtime +14 -delete
